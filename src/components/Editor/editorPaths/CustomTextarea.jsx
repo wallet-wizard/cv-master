f27ebf10fc3efc1eede56645349332cffbaea89b
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2024-01-18 00:56:33 +0000

    Improves CustomTextarea component's state functionality

--- src/components/Editor/editorPaths/CustomTextarea.jsx
+++ src/components/Editor/editorPaths/CustomTextarea.jsx
@@ -1,24 +1,62 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useId, useRef, useEffect, useState } from 'react';
 
-function CustomTextarea() {
-  const [columns, setColumns] = useState([]);
+function CustomTextarea(props) {
+  const id = useId();
+  const textAreaRef = useRef(null)
+  const startingRows = useRef(null)
+  console.log(id)
   const [rowsCount, setRowsCount] = useState(0);
+  
+  useEffect(() => {
+    if (textAreaRef.current) {
+      const lineHeight = parseInt(getComputedStyle(textAreaRef.current).lineHeight, 10);
+      const rows = Math.floor(textAreaRef.current.scrollHeight / lineHeight);
+      console.log(`Text spans approximately ${rows} rows.`);
+      startingRows.current = rows
+      setRowsCount(rows)
+    }
+  }, [])
 
-  function handleTextareaChange(event) {
-    const inputText = event.target.value;
-    const newRows = inputText.split('\n');
-    setRowsCount(newRows.length)
+  function updateRows(event) {
+    if (textAreaRef.current) {
+      const startingRowsLength = startingRows.current;
+      const lineHeight = parseInt(getComputedStyle(textAreaRef.current).lineHeight, 10);
+      const rows = Math.floor(textAreaRef.current.scrollHeight / lineHeight);
+
+      console.log("starting Rows:", startingRowsLength)
+      console.log("actual Rows:", rows)
+      console.log("prev Rows:", rowsCount)
+
+      if (rows < startingRowsLength) {
+        return setRowsCount(startingRowsLength);
+      }
+      
+      if (rows > startingRowsLength && rows > rowsCount) {
+        return setRowsCount(rows);
+      } else if (rows === rowsCount){
+        return setRowsCount(rowsCount - 1)
+      } else {
+        return setRowsCount(rows - 1);
+      }
+
+
+    }
   };
 
-  console.log(columns) 
-  return (
 
-      <textarea
-        onChange={handleTextareaChange}
-        placeholder="Type your columns here, each line is a new column"
-        className='editor-textArea'
-        rows={rowsCount > 2 && rowsCount}
-      />
+  return (
+      <div>
+        <label htmlFor={`${id}-${props.id ? props.id : ""}`}></label>
+        <textarea
+          ref={textAreaRef}
+          id={`${id}-${props.id ? props.id : ""}`}
+          onChange={updateRows}
+          placeholder="Enter Title (no more than 2 lines recommended)"
+          className='editor-textArea'
+          rows={(rowsCount > props.rows) ? rowsCount : props.rows}
+          defaultValue={props.defaultValue ? props.defaultValue : ""}
+        />
+      </div>
 
   );
 };
--- src/components/Editor/editorPaths/EditorBasicInfo.jsx
+++ src/components/Editor/editorPaths/EditorBasicInfo.jsx
@@ -2,12 +2,33 @@ import React from 'react';
 import '../editor.css';
 import CustomTextarea from './CustomTextarea';
 
+
+
 export default function EditorBasicInfo() {
     return(
         <div className="d-block Editor-BasicInfo">
             <h3>New CV</h3>
-            <CustomTextarea />
-            <textarea className=" editor-textArea" rows={10}/>
+            <CustomTextarea id="title" rows={titleLength} defaultValue={title}/>
+            <CustomTextarea id="summary" rows={summaryLength} defaultValue={summary}/>
         </div>
     )
 }
+
+
+// Default Values
+
+// TITLE
+const title = `# Enter Name
+## Front End Developer
+____
+`
+const titleLength = title.split('\n').length;
+
+
+// SUMMARY
+const summary = (
+    `## Summary
+
+Results-oriented and highly skilled Front-end Developer with [X years] of experience creating and implementing innovative web designs and user experiences. Proficient in HTML, CSS, JavaScript, and modern frameworks. Adept at collaborating with cross-functional teams to drive project success. `
+    )
+const summaryLength = summary.split('\n').length;
