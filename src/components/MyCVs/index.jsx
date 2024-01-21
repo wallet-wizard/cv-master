5ef82f3030ed0ea41236bb9bf5b1b5141af56b11
Josef K <ahmed.abshir91@gmail.com>
2024-01-21 23:38:48 +0000

    Updates MyCVs component - Adds tabe structure and styling

--- src/components/MyCVs/index.jsx
+++ src/components/MyCVs/index.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import './myCVs.css'
+
 
 export default function MyCVs(){
     return(
-        <h1>Hello from MyCVs!</h1>
+        <div id='page-div'>
+            <div id='table-div' className='p-5'>
+                <table className="table table-striped"> 
+                    <thead>
+                        <tr>
+                        <th className='top-left text-center'>Company</th>
+                        <th className='text-center'>CV</th>
+                        <th className='text-center'>Cover Letter</th>
+                        <th className='top-right text-center'>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody className='py-3'>
+                        <tr>
+                        <td>CV Master</td>
+                        <td>Intern-CV.pdf</td>
+                        <td>Obviously</td>
+                        <td>Under Review</td>
+                        </tr>
+
+                        <tr>
+                        <td>CV Master</td>
+                        <td>Intern-CV.pdf</td>
+                        <td>Obviously</td>
+                        <td>Under Review</td>
+                        </tr>
+                    </tbody>
+                    </table>
+                    <div id='btn-div'>
+                        <button className='btn btn-primary' id='add-application'>Add Application</button>
+                    </div>
+                </div>
+        </div>
+        
     )
 }
