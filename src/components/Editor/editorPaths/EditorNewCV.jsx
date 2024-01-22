bdc5b6a8aedbfd71721fe523eeb6c310d58e86bb
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2024-01-22 22:00:34 +0000

    Improves functionality on NewCV page

--- src/components/Editor/editorPaths/EditorNewCV.jsx
+++ src/components/Editor/editorPaths/EditorNewCV.jsx
@@ -1,24 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../editor.css';
 import CustomTextarea from './CustomTextarea';
-import { useGlobalContext } from '../../../utils/GlobalContext'
-
+import { useNavigate } from 'react-router-dom';
+import { useGlobalContext } from '../../../utils/GlobalContext';
 
 export default function EditorNewCV(props) {
-
-  const { setText, userData, setUserData, capitalize } = useGlobalContext()
+  const { setText, userData, setUserData } = useGlobalContext();
   const value = userData.stagingCVTitle;
-  console.log(userData.stagingCVTitle)
+  const navigate = useNavigate();
+  const inputRef = useRef(null); // Create a ref for the input element
+
+  // LOGIC for submit
+  function handleGetStarted(e) {
+    e.preventDefault();
+    console.log('No Title value inserted!');
+    if (value) {
+      navigate('basic-info');
+    }
+  }
+
+  // Use useEffect to focus on the input when the component mounts
+  useEffect(() => {
+    if (!value && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [value]);
+
   return (
-    <form className="d-flex flex-column editor-NewCV text-center justify-content-center align-items-center">
+    <form onSubmit={(e) => handleGetStarted(e)} className="d-flex flex-column editor-NewCV text-center justify-content-center align-items-center">
       <h3 className='m-5'> Let's create your new CV!</h3>
       <div className='container-fluid input-div mb-3 p-0 d-flex '>
         <label className='col-3 col-sm-2' htmlFor="CVTitle">Title :</label>
-        <input name='CVTitle' className='col' value={value} onChange={(event) => setText({event, setState: setUserData})} id="username" type="text" />
+        <input
+          ref={inputRef} // Assign the ref to the input element
+          name='CVTitle'
+          className='col'
+          value={value}
+          onChange={(event) => setText({ event, setState: setUserData })}
+          id="username"
+          type="text"
+        />
       </div>
-      <button className='btn btn-get-started' type="button">GET STARTED</button>
+      <button className='btn btn-get-started' type="submit">GET STARTED</button>
     </form>
-  )
+  );
 }
-
-
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useGlobalContext } from '../../utils/GlobalContext';
-import { useNavigate } from 'react-router-dom';
 
 export default function Login(props) {
 
-  const [ inputValue, setInputValue ] = useState(null)
-  const { logout, authenticated, updateCVMCurrentUser } = useGlobalContext()
-  const navigate = useNavigate()
-  
+  const [inputValue, setInputValue] = useState(null)
+  const { logout, authenticated, updateCVMCurrentUser } = useGlobalContext();
+
   // LOGIC for submit
   function submitUser(e) {
     e.preventDefault()
-    
+
     if (!inputValue.trim()) {
       return;
     }
@@ -33,21 +31,21 @@ export default function Login(props) {
 
   return (
     <div>
-    {!authenticated ? (
-      <>
-        <h1>Hello from Login!</h1>
-        <form onSubmit={(e) => submitUser(e)}>
-          <label htmlFor="username">Type your user name:</label>
-          <input onChange={(e) => handleChange(e)} id="username" type="text" />
-          <button type="submit">LOGIN</button>
-        </form>
-      </>
-    ) : (
-      <>
-        <h1>Hi, user!</h1>
-        <button onClick={logout} className="logout">Log out?</button>
-      </>
-    )}
-  </div>
+      {!authenticated ? (
+        <>
+          <h1>Hello from Login!</h1>
+          <form onSubmit={(e) => submitUser(e)}>
+            <label htmlFor="username">Type your user name:</label>
+            <input onChange={(e) => handleChange(e)} id="username" type="text" />
+            <button type="submit">LOGIN</button>
+          </form>
+        </>
+      ) : (
+        <>
+          <h1>Hi, user!</h1>
+          <button onClick={logout} className="logout">Log out?</button>
+        </>
+      )}
+    </div>
   )
 }
