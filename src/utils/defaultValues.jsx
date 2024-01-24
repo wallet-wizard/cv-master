76ef9533548df0fdbd6261bf5ae66459f566e218
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2024-01-24 20:32:06 +0000

    Improves default CV

--- src/utils/defaultValues.jsx
+++ src/utils/defaultValues.jsx
@@ -5,7 +5,9 @@
  */
 
 export const stageTitle = (`# Enter Name
-## Front End Developer
+### Front End Developer
+
+📧 Email: john.doe@example.com | 🌐 Portfolio: [john-doe.dev](https://www.john-doe.dev) | 📱 LinkedIn: [linkedin.com/in/johndoe](https://www.linkedin.com/in/johndoe) | 📞 Phone: (555) 123-4567
 ____
 `
 );
@@ -13,12 +15,16 @@ ____
 export const stageSummary = (
     `## Summary
 
-Results-oriented and highly skilled Front-end Developer with [X years] of experience creating and implementing innovative web designs and user experiences. Proficient in HTML, CSS, JavaScript, and modern frameworks. Adept at collaborating with cross-functional teams to drive project success. `
+Enthusiastic and motivated Junior Front End Developer with a passion for creating dynamic and responsive web applications. Recently graduated from a rigorous coding bootcamp, equipped with solid foundations in HTML, CSS, JavaScript, and modern frameworks like React. A quick learner who is eager to contribute to innovative projects and collaborate with like-minded professionals.
+
+____`
 );
 
 export const stageSkills = [
-    "1skill", "2skill", "3skill", "4skill", "5skill"
-
+  `- **Languages:** HTML, CSS, JavaScript`,
+  `- **Frameworks/Libraries:** React, Vite`,
+  `- **Responsive Design:** Bootstrap, ....`,
+  "____"
 ]
 
 export const stageExp = [
@@ -28,18 +34,50 @@ export const stageExp = [
 - Collaborated with the design team to implement pixel-perfect and responsive user interfaces.
 - Implemented performance optimizations, reducing page load times by [percentage].
 - Mentored junior developers, conducting code reviews and providing technical guidance.
-`]
+`,
+"____"
+]
 
 export const stageEdu = [
-    `### Front End Developer Bootcamp - edX | University of Birmongham | 2024`,
-    `### E[ducation Type] | [University Name] | [Graduation Year]`,
-    `### E[ducation Type] | [University Name] | [Graduation Year]`
+
+    `### Coding Bootcamp Graduate
+**Tech Academy Bootcamp**
+*City, State*
+*Month Year - Month Year*
+    
+- Completed an intensive full-stack development bootcamp.
+
+- Developed proficiency in HTML5, CSS3, JavaScript, React, and other modern web technologies.
+
+ - Worked on real-world projects and participated in team-based coding challenges.
+ `,
+ "____"
 ]
+
+export const stageOther = (
+` ## Projects
+
+### Portfolio Website
+- Designed and developed a personal portfolio website to showcase projects and skills.
+- Implemented responsive design for optimal viewing on various devices.
+- Technologies used: HTML5, CSS3, JavaScript.
+
+
+## Github Repositories
+- [GitHub Profile](https://github.com/johndoe)
+  - Repositories showcasing personal and collaborative projects.
+
+
+## References
+Dan Mueller
+`
+)
+
 
 export const initialStaging = {
     title: stageTitle,
     summary: stageSummary,
-    other: '',
+    other: stageOther,
     skills: {
       header: '## Skills',
       skills: stageSkills
